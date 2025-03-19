e9f4f536fa4d80eb6eeb9be9824c1d5287d8c92c
CaoMeiYouRen <996881204@qq.com>
2025-03-19 22:33:16 +0800

    refactor(api): 优化API代码结构
    - 注释掉afdian webhook中的事务处理代码
    - 重命名test.ts为index.ts
    - 简化index.ts中的返回数据结构，添加status字段

--- server/api/afdian/webhook.post.ts
+++ server/api/afdian/webhook.post.ts
@@ -1,19 +1,19 @@
 export default defineEventHandler(async (event) => {
     const body = await readBody(event)
 
-    await event.context.prisma.$transaction([
-        event.context.prisma.order.update({
-            where: { id: body.data.order.custom_order_id },
-            data: {
-                afdian_id: body.data.order.out_trade_no,
-                status: body.data.order.status === 2 ? 'PAID' : 'FAILED',
-                paid_at: new Date(),
-            },
-        }),
-        event.context.prisma.webhookLog.create({
-            data: { payload: JSON.stringify(body) },
-        }),
-    ])
+    // await event.context.prisma.$transaction([
+    //     event.context.prisma.order.update({
+    //         where: { id: body.data.order.custom_order_id },
+    //         data: {
+    //             afdian_id: body.data.order.out_trade_no,
+    //             status: body.data.order.status === 2 ? 'PAID' : 'FAILED',
+    //             paid_at: new Date(),
+    //         },
+    //     }),
+    //     event.context.prisma.webhookLog.create({
+    //         data: { payload: JSON.stringify(body) },
+    //     }),
+    // ])
 
     return { ec: 200, em: '' }
 })
---
+++ server/api/index.ts
@@ -0,0 +1,1 @@
+export default defineEventHandler(async (event) => ({ message: 'Hello World!', status: 200 }))

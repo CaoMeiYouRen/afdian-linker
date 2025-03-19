5f259582b11b0934d5439283b741ac1c4214c6f2
CaoMeiYouRen <996881204@qq.com>
2025-03-19 23:58:39 +0800

    chore(eslint): 根据环境设置警告级别，调整no-undef和no-console规则

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,17 @@
+const __ERROR__ = process.env.NODE_ENV === 'production' ? 2 : 0
+const __WARN__ = process.env.NODE_ENV === 'production' ? 1 : 0
 module.exports = {
     root: true,
     extends: [
         'cmyr/vue3',
     ],
     rules: {
-        'no-undef': [0],
+        'no-undef': [__WARN__],
         'implicit-arrow-linebreak': [1],
         'import/no-named-as-default': [0],
         'vue/require-name-property': [0],
         '@typescript-eslint/ban-types': [1],
         'vue/multi-word-component-names': [0],
-        'no-console': [1],
+        'no-console': [__WARN__],
     },
 }

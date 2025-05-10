a76aaef65c331fe24a6a9f2450bb69befe455310
CaoMeiYouRen <996881204@qq.com>
2025-05-10 18:05:07 +0800

    fix(profile): 仅当 auth0Id 存在时显示第三方账号信息

--- pages/profile.vue
+++ pages/profile.vue
@@ -35,6 +35,19 @@
                                 <v-list-item-subtitle>{{ userStore.userInfo?.username }}</v-list-item-subtitle>
                             </v-list-item>
 
+                            <!-- 仅当auth0Id存在时显示 -->
+                            <v-list-item v-if="userStore.userInfo?.auth0Id">
+                                <template #prepend>
+                                    <v-icon color="primary">
+                                        mdi-fingerprint
+                                    </v-icon>
+                                </template>
+                                <v-list-item-title>第三方账号</v-list-item-title>
+                                <v-list-item-subtitle class="pb-1">
+                                    {{ userStore.userInfo?.auth0Id }}
+                                </v-list-item-subtitle>
+                            </v-list-item>
+
                             <v-list-item>
                                 <template #prepend>
                                     <v-icon color="primary">

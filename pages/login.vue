f5ded218af38700429d1b4062aafd96182f786b4
CaoMeiYouRen <996881204@qq.com>
2025-05-13 23:46:02 +0800

    fix(login): 修复登录页面容器宽度，确保在不同状态下适配

--- pages/login.vue
+++ pages/login.vue
@@ -243,7 +243,8 @@ onMounted(async () => {
 
 .login-container {
     min-height: 100vh;
-    width: 100vw;
+    min-width: 100vw;
+    width: 100%;
     display: flex;
     align-items: center;
     justify-content: center;
